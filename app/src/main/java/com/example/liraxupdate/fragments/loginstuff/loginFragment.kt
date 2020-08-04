e1a157467cc93cf8966da5bcf1a8de509f756985
Dev Kumar <dev.webcreatars@gmail.com>
2020-08-04 18:31:18 +0530

    second commit

--- app/src/main/java/com/example/liraxupdate/fragments/loginstuff/loginFragment.kt
+++ app/src/main/java/com/example/liraxupdate/fragments/loginstuff/loginFragment.kt
@@ -9,14 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import com.example.liraxupdate.Activities.DashboardActivity
-import com.example.liraxupdate.Activities.SignUpActivity
+
 import com.example.liraxupdate.R
 import com.example.liraxupdate.databinding.FragmentLoginBinding
 import com.example.liraxupdate.viewmodels.LoginViewModel
 
-import kotlinx.android.synthetic.main.fragment_login.*
+
 
 
 class loginFragment : Fragment() {
@@ -27,8 +25,7 @@ class loginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-     val     binding : FragmentLoginBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+     val     binding : FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
         binding.loginframodel= viewModel
         binding.lifecycleOwner = this
